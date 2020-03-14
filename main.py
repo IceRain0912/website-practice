207e11a132f4018c5593a6527d3993df19a1051c
IceRain0912 <61371712+IceRain0912@users.noreply.github.com>
2020-03-14 11:07:38 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import glob
+import os
+from flask import Flask, render_template
+server = Flask("IceRain")
+
+@server.route("/")
+def index():
+  ds = glob.glob("gamer/*")
+  ds = [d.split("/")[-1] for d in ds]
+  return render_template("index.html", dirs=ds)
+
+@server.route("/gamer/<c>")
+def gamer(c):
+  fs = glob.glob("gamer/" + c + "/*.txt")
+  contents = []
+  for i,fn in enumerate(fs):
+    name = os.path.split(fn)[-1].replace(".txt", "")
+    f = open(fn)
+    gamer = f.read()
+    f.close()
+    contents.append((name,gamer,i))
+  return render_template("gamer.html", cs=contents)
+
+server.run(debug=True,
+          host = "0.0.0.0",
+          port = "3000")
